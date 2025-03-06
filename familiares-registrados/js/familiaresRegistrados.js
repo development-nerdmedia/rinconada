17ab1a310fccc827d35a0d425fa1f05b3127284c
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2025-03-06 13:34:17 -0500

    Add files via upload

--- familiares-registrados/js/familiaresRegistrados.js
+++ familiares-registrados/js/familiaresRegistrados.js
@@ -35,4 +35,60 @@ document.addEventListener("DOMContentLoaded", function () {
       item.classList.add("active");
     });
   });
+
+  const items3 = document.querySelectorAll(
+    ".modalPreferencias .content .elemento"
+  );
+  items3.forEach((item) => {
+    const flecha = item.querySelector(".cabecera");
+
+    if (!flecha) return;
+
+    flecha.addEventListener("click", function (e) {
+      e.preventDefault();
+      if (item.classList.contains("active")) {
+        item.classList.remove("active");
+        return;
+      }
+      items3.forEach((el) => el.classList.remove("active"));
+      item.classList.add("active");
+    });
+  });
+
+  document
+    .querySelectorAll(".modalPreferencias .listaInputs .contentInput label")
+    .forEach((label) => {
+      label.addEventListener("click", function (event) {
+        let contentInput = this.closest(".contentInput");
+
+        // Si ya tiene la clase "active", la quitamos, sino, la añadimos
+        if (contentInput.classList.contains("active")) {
+          contentInput.classList.remove("active");
+          contentInput.querySelector("input").checked = false; // Desmarcar el input
+        } else {
+          contentInput.classList.add("active");
+          contentInput.querySelector("input").checked = true; // Marcar el input
+        }
+
+        event.preventDefault(); // Evita que se active el radio automáticamente
+      });
+    });
+
+  document.querySelectorAll(".editarPreferencia").forEach((btn) => {
+    btn.addEventListener("click", function () {
+      document.querySelector(".modalPreferencias").classList.add("active");
+    });
+  });
+
+  document
+    .querySelector(".closeModalPreferencias")
+    .addEventListener("click", function () {
+      document.querySelector(".modalPreferencias").classList.remove("active");
+    });
+
+  document
+    .querySelector(".modalPreferencias .content .guardar")
+    .addEventListener("click", function () {
+      document.querySelector(".modalPreferencias").classList.remove("active");
+    });
 });
