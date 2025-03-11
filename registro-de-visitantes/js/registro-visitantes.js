48d9fe851f602aa98cd3c831d6da2920a7dcb86a
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2025-03-11 03:00:16 -0500

    Add files via upload

--- registro-de-visitantes/js/registro-visitantes.js
+++ registro-de-visitantes/js/registro-visitantes.js
@@ -76,4 +76,69 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
+
+  $(function () {
+    $.datepicker.regional["es"] = {
+      closeText: "Cerrar",
+      prevText: "Anterior",
+      nextText: "Siguiente",
+      currentText: "Hoy",
+      monthNames: [
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre",
+      ],
+      monthNamesShort: [
+        "Ene",
+        "Feb",
+        "Mar",
+        "Abr",
+        "May",
+        "Jun",
+        "Jul",
+        "Ago",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dic",
+      ],
+      dayNames: [
+        "Domingo",
+        "Lunes",
+        "Martes",
+        "Miércoles",
+        "Jueves",
+        "Viernes",
+        "Sábado",
+      ],
+      dayNamesShort: ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"],
+      dayNamesMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sá"],
+      weekHeader: "Sm",
+      dateFormat: "dd-mm-yy",
+      firstDay: 1,
+      isRTL: false,
+      showMonthAfterYear: false,
+      yearSuffix: "",
+    };
+
+    $.datepicker.setDefaults($.datepicker.regional["es"]);
+
+    $("#datepicker").datepicker({
+      beforeShow: function () {
+        $("body").addClass("modal"); // Agrega la clase cuando se abre
+      },
+      onClose: function () {
+        $("body").removeClass("modal"); // Elimina la clase modal cuando se cierra el Datepicker
+      },
+    });
+  });
 });
